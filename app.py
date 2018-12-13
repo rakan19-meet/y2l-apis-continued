99b17828448cf63e70d1afea44d06918dbcb6c89
aboud20-meet <aboud20@meet.mit.edu>
2018-12-13 18:49:59 +0200

    lab9

--- app.py
+++ app.py
@@ -9,14 +9,26 @@ def home():
 def study_image():
     
     image_url = request.form['url-input']
-    # At this point you have the image_url value from the front end
-    # Your job now is to send this information to the Clarifai API
-    # and read the result, make sure that you read and understand the
-    # example we covered in the slides! 
+    headers = {'Authorization': 'Key f2f339a3cc374420a221fa27e58a3202'}
 
-    # YOUR CODE HERE!
+    # this is the url of where your request will go
+    api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+
+    # this is content of the message(data) you are sending to clarifai
+    data ={"inputs": [
+        {
+            "data": {
+            "image": {
+                "url": image_url
+            }
+            }
+        }
+        ]}
+
+    # putting everything together; sending the request!
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
     
-    return render_template('home.html', results="No results yet :(")
+    return render_template('home.html', results=response)
 
 if __name__ == '__main__':
     app.run(debug=True)
